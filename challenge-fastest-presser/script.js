235ecbb7ef65b3222c7ff867cd8987b6f9d9c00d
PatOn <mahomed2278@gmail.com>
2021-02-06 12:22:54 +0000

    input validator
    input validator
    start game - frame

--- challenge-fastest-presser/script.js
+++ challenge-fastest-presser/script.js
@@ -1,4 +1,4 @@
-function startGame() {}
+// function startGame() { }
 
 function keyBoardEvents(e) {
   if (e.keyCode === 83) {
@@ -9,3 +9,29 @@ function keyBoardEvents(e) {
 }
 
 document.addEventListener("keypress", keyBoardEvents);
+
+
+
+//variables
+const input = document.querySelector(".input") //wrong one -> put a proper one
+
+
+//helper function
+const inputReaderValidator = function () {
+  const input = 21;
+  // const input = input.value;
+  if (input === "" || Number.isNaN(input) || input < 20) {
+    //render the warning in html
+  }
+  // return input
+  return input;
+};
+
+
+const startGame = function () {
+  const userInput = inputReaderValidator(); //
+  // input validation
+  const timeOfTheGame = userInput * 1000; // setTimeout is taking input in milliseconds so * 1000
+
+
+};
