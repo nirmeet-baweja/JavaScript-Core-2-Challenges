99d7777fdccd09849a29a6022a420339b76d7450
nirmeet-baweja <73722230+nirmeet-baweja@users.noreply.github.com>
2021-02-06 12:25:38 +0000

    v1 - countdown function

--- challenge-fastest-presser/script.js
+++ challenge-fastest-presser/script.js
@@ -1,11 +1,33 @@
+let ountS = 0;
+let countL = 0;
+
 function startGame() {}
 
 function keyBoardEvents(e) {
   if (e.keyCode === 83) {
     // On 'S' Pressed
+    countS++;
   } else if (e.keyCode === 76) {
     // On 'L' Pressed
+    countL++;
   }
 }
 
-document.addEventListener("keypress", keyBoardEvents);
+// document.addEventListener("keypress", keyBoardEvents);
+
+function countdown(countDownValue) {
+
+  document.addEventListener("keypress", keyBoardEvents);
+
+  let intervalId = setInterval(function() {
+      if(countDownValue >= 0) {
+        countDownValue--;
+        console.log("Timer decremented!");
+      }
+      if(timer === -1) {
+        clearInterval(intervalId);
+        document.removeEventListener("", keyBoardEvents);
+        isGameOver = true;
+      }
+    }, 1000);
+}
