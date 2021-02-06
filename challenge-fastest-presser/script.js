b2fcc8d87481efb46acd65d23fdc80ef089c2f94
PatOn <mahomed2278@gmail.com>
2021-02-06 15:19:24 +0000

    Spinner

--- challenge-fastest-presser/script.js
+++ challenge-fastest-presser/script.js
@@ -18,7 +18,7 @@ function keyBoardEvents(e) {
 const inputReaderValidator = function (inputValue) {
   if (inputValue === "" || Number.isNaN(inputValue) || inputValue > 20 || inputValue <= 0) {
     //render the warning in html
-    console.log("Input value is invalid");
+    console.error("Input value is invalid");
     return false;
   }
   return true;
@@ -27,52 +27,82 @@ const inputReaderValidator = function (inputValue) {
 
 const startGame = function () {
   //variables
+  document.querySelector("#btnStart").removeEventListener("onclick", startGame)
   const input = document.querySelector("#time") //wrong one -> put a proper one
-  
+
   // input validation
   const isInputValid = inputReaderValidator(input.value); //
-  
-  if(isInputValid) { 
+  spinnerAdding();
+  if (isInputValid) {
     countdown(input.value);
   }
-  // the esle part needs to dealt with
+  // the else part needs to dealt with
+
+  // block the start function
+
 };
+
+//Because Time out is async function we should put it after countdown would end
+const originalGameState = function () {
+  countL = countS = 0;
+  document.querySelector("#time").value = "";
+  document.removeEventListener("keypress", keyBoardEvents);
+
+};
+
+const spinnerAdding = function () {
+  const markup = `    
+  <div class="spinner">
+
+            <img src="spinner.jpeg" alt="spinner">
+
+        </div>`
+  document.querySelector(".content").insertAdjacentHTML("afterbegin", markup);
+
+};
+
+const spinnerRemoving = function () {
+  document.querySelector(".spinner").remove();
+}
 
 // document.addEventListener("keypress", keyBoardEvents);
 function declareWinner(userSCounter, userLCounter) {
-	let winner;
-	if (userSCounter > userLCounter) {
-		console.log("Player S wins");
-		winner = 'playerS';
-	} else if (userSCounter < userLCounter) {
-		console.log("Player L wins");
-		winner = 'playerL';
-	} else {
-		console.log("It's a tie");
-	}
-  const theWinner=document.getElementById(winner);
-  theWinner.style.backgroundColor="green";
-	// const confettiSettings = { target: winner };
-	// const confetti = new ConfettiGenerator(confettiSettings);
-	// confetti.render();
+  let winner;
+  if (userSCounter > userLCounter) {
+    console.log("Player S wins");
+    winner = 'playerS';
+  } else if (userSCounter < userLCounter) {
+    console.log("Player L wins");
+    winner = 'playerL';
+  } else {
+    console.log("It's a tie");
+    winner = ""
+  }
+  const theWinner = document.getElementById(winner);
+  // no more error when there is tie
+  document.getElementById(winner) ? theWinner.style.backgroundColor = "green" : "";
+  // const confettiSettings = { target: winner };
+  // const confetti = new ConfettiGenerator(confettiSettings);
+  // confetti.render();
 }
 
 function countdown(countDownValue) {
 
   document.addEventListener("keypress", keyBoardEvents);
 
-  let intervalId = setInterval(function() {
-      if(countDownValue >= 0) {
-        countDownValue--;
-        console.log("Timer decremented!");
-      }
-      if(countDownValue === -1) {
-        clearInterval(intervalId);
-        document.removeEventListener("", keyBoardEvents);
-        declareWinner(countS,countL);
-        isGameOver = true;
-      }
-    }, 1000);
+  let intervalId = setInterval(function () {
+    if (countDownValue >= 0) {
+      countDownValue--;
+      console.log("Timer decremented!");
+    }
+    if (countDownValue === -1) {
+      clearInterval(intervalId);
+      document.removeEventListener("", keyBoardEvents);
+      declareWinner(countS, countL);
+      isGameOver = true;
+
+    }
+  }, 1000);
 }
 
 
